04a9efdd4f83dd979b4d63c947c9eb7553a02099
Kirill71 <extazy1701@gmail.com>
2016-10-02 10:34:31 +0300

    counting sort implementation

--- Sorting/countingsort.cpp
+++ Sorting/countingsort.cpp
@@ -2,9 +2,9 @@
 #include <array>
 #include<vector>
 
-
 template< typename T, unsigned SIZE>
 void countingsort(std::array<T, SIZE> a, std::array<T, SIZE>& b);
+
 int main() {
 	std::array<int, 8> a{2,5,3,0,2,3,0,3};
 	std::array<int, 8> b;
@@ -26,7 +26,7 @@ void countingsort(std::array<T, SIZE> a, std::array<T, SIZE>& b) {
 	for (unsigned i = 1; i < c.size(); ++i) {
 		c[i] += c[i - 1]; // counting elemnts not more i
 	}
-
+	
 	for (int j = a.size() - 1; j >= 0; --j) {
 		b[c[a[j]] - 1] = a[j]; // write elemrnts to result array
 		--c[a[j]];
